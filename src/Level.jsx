2b386a650c48859e861c3258496db9d1c307eac0
corentin-desilles <corentindesilles974@gmail.com>
2024-01-22 14:31:38 +0100

    materials

--- src/Level.jsx
+++ src/Level.jsx
@@ -2,6 +2,11 @@ import * as THREE from 'three'
 
 const boxGeometry = new THREE.BoxGeometry(1,1,1)
 
+const floor1Material = new THREE.MeshStandardMaterial({ color: 'limegreen' })
+const floor2Material = new THREE.MeshStandardMaterial({ color: 'greenyellow' })
+const obstacleMaterial = new THREE.MeshStandardMaterial({ color: 'orangered' })
+const wallMaterial = new THREE.MeshStandardMaterial({ color: 'slategrey' })
+
 function BlockStart({ position = [0, 0, 0] })
 {
     return <group position={position}>
@@ -9,12 +14,12 @@ function BlockStart({ position = [0, 0, 0] })
         {/* Floor */}
         <mesh 
             geometry={boxGeometry} 
+            material={floor1Material}
             position={ [ 0, - 0.1, 0 ] } 
             scale={[4, 0.2, 4]} 
-            receiveShadow>
-             <meshStandardMaterial color="limegreen" />
-        </mesh>
-        
+            receiveShadow
+        />
+
     </group>
 }
 
