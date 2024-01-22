1f4089ff74fecb61966239f75ad0ab942c5d61e4
corentin-desilles <corentindesilles974@gmail.com>
2024-01-22 18:00:07 +0100

    controls

--- src/Player.jsx
+++ src/Player.jsx
@@ -1,10 +1,50 @@
 import { RigidBody } from "@react-three/rapier"
+import { useFrame } from "@react-three/fiber"
+import { useKeyboardControls } from "@react-three/drei" //this hook return a function to subscribe to key changes and a function to get the current state of the keys
+import { useRef } from "react"
 
 export default function Player() {
 
+    const body = useRef()
+    const [subscribeKeys, getKeys] = useKeyboardControls()
+
+        useFrame((state, delta) =>
+        {
+            const { forward, backward, leftward, rightward } = getKeys()
+
+            const impulse = {x: 0, y: 0, z: 0}
+            const torque = {x: 0, y: 0, z: 0}
+
+            const impulseStrength = 0.6 * delta
+            const torqueStrength = 0.2 * delta
+
+            if(forward) {
+                impulse.z -= impulseStrength
+                torque.x -= torqueStrength
+            }
+
+            if(backward) {
+                impulse.z += impulseStrength
+                torque.x += torqueStrength
+            }
+
+            if(leftward) {
+                impulse.x -= impulseStrength
+                torque.z += torqueStrength
+            }
+
+            if(rightward) {
+                impulse.x += impulseStrength
+                torque.z -= torqueStrength
+            }
+
+            body.current.applyImpulse(impulse)
+            body.current.applyTorqueImpulse(torque)
+        })
    
     return <>
     <RigidBody
+        ref={body}
         canSleep={false}
         position={[0, 1, 0]}
         colliders="ball"
