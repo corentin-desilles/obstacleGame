72dd08637bdf7504bf26a20fe787904c6ded6b37
corentin-desilles <corentindesilles974@gmail.com>
2024-01-23 10:56:29 +0100

    toggle restart btn

--- src/Interface.jsx
+++ src/Interface.jsx
@@ -1,8 +1,12 @@
 import { useKeyboardControls } from "@react-three/drei"
+import useGame from "./stores/useGame"
 
 
 export default function Interface()
 {
+    const restart = useGame((state) => state.restart)
+    const phase = useGame((state) => state.phase)
+
     const forward = useKeyboardControls((state) => state.forward)
     const backward = useKeyboardControls((state) => state.backward)
     const leftward = useKeyboardControls((state) => state.leftward)
@@ -16,9 +20,11 @@ export default function Interface()
         </div>
 
         {/* Restart */}
-        <div className="restart">
-            Restart
-        </div>
+        {phase === 'ended' && 
+            <div className="restart" onClick={restart}>
+                Restart
+            </div>
+        }
 
         {/* Controls */}
         <div className="controls">
