4c7e53839e7e4e0212321cf1b36ffe7ca1c1b9c7
corentin-desilles <corentindesilles974@gmail.com>
2024-01-24 12:37:29 +0100

    sound effect test

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,25 +1,22 @@
-import {Physics} from '@react-three/rapier'
+import { Physics } from '@react-three/rapier'
 import Lights from './Lights.jsx'
-import {Level} from './Level.jsx'
+import { Level } from './Level.jsx'
 import Player from './Player.jsx'
 import useGame from './stores/useGame.jsx'
 
-export default function Experience()
-{
-    const blocksCount = useGame((state) =>  state.blocksCount)
-    const blocksSeed = useGame(state =>  state.blocksSeed)
+export default function Experience() {
+    const blocksCount = useGame(state => state.blocksCount)
+    const blocksSeed = useGame(state => state.blocksSeed)
 
-    return <>
+    return (
+        <>
+            <color args={['#bdedfc']} attach="background" />
 
-        <color 
-            args={['#bdedfc']} 
-            attach="background"
-        />
-
-        <Physics debug={ false }>
-            <Lights />
-            <Level count={blocksCount} seed={blocksSeed} />
-            <Player />
-        </Physics>
-    </>
+            <Physics debug={false}>
+                <Lights />
+                <Level count={blocksCount} seed={blocksSeed} />
+                <Player />
+            </Physics>
+        </>
+    )
 }
--- src/Player.jsx
+++ src/Player.jsx
@@ -1,161 +1,155 @@
-import { useRapier, RigidBody } from "@react-three/rapier"
-import { useFrame } from "@react-three/fiber"
-import { useKeyboardControls } from "@react-three/drei" //this hook return a function to subscribe to key changes and a function to get the current state of the keys
-import { useEffect, useRef, useState } from "react"
+import { useRapier, RigidBody } from '@react-three/rapier'
+import { useFrame } from '@react-three/fiber'
+import { useKeyboardControls } from '@react-three/drei' //this hook return a function to subscribe to key changes and a function to get the current state of the keys
+import { useEffect, useRef, useState } from 'react'
 import * as THREE from 'three'
-import useGame from "./stores/useGame"
+import useGame from './stores/useGame'
+// import { Howl, Howler } from 'howler'
 
 export default function Player() {
-
     const body = useRef()
     const [subscribeKeys, getKeys] = useKeyboardControls()
-    const {rapier, world} = useRapier()
+    const { rapier, world } = useRapier()
 
-    const [smoothedCameraPosition] = useState(() => new THREE.Vector3(10, 10, 10))
+    // const playCollisionSound = () => {
+    //     const collisionSound = new Howl({
+    //         src: ['../sounds/jumpland48000.mp3'],
+    //         volume: 0.5,
+    //     })
+
+    //     collisionSound.play()
+    // }
+
+    const [smoothedCameraPosition] = useState(
+        () => new THREE.Vector3(10, 10, 10)
+    )
     const [smoothedCameraTarget] = useState(() => new THREE.Vector3())
 
-    const start = useGame((state) => state.start)
-    const end = useGame((state) => state.end)
-    const restart = useGame((state) => state.restart)
-    const blocksCount = useGame((state) => state.blocksCount)
+    const start = useGame(state => state.start)
+    const end = useGame(state => state.end)
+    const restart = useGame(state => state.restart)
+    const blocksCount = useGame(state => state.blocksCount)
 
-    const jump = () =>
-    {
+    const jump = () => {
         const origin = body.current.translation()
         origin.y -= 0.31
-        const direction = {x: 0, y: - 1, z: 0}
+        const direction = { x: 0, y: -1, z: 0 }
         const ray = new rapier.Ray(origin, direction)
         const hit = world.castRay(ray, 10, true)
 
-        if(hit.toi < 0.15)
-        {
-            body.current.applyImpulse({x: 0, y: 0.5, z: 0})
+        if (hit.toi < 0.15) {
+            body.current.applyImpulse({ x: 0, y: 0.5, z: 0 })
+            // playCollisionSound()
         }
     }
 
-    const reset = () =>
-    {
-        body.current.setTranslation({x: 0, y: 1, z: 0})
-        body.current.setLinvel({x: 0, y: 0, z: 0})
-        body.current.setAngvel({x: 0, y: 0, z: 0})
+    const reset = () => {
+        body.current.setTranslation({ x: 0, y: 1, z: 0 })
+        body.current.setLinvel({ x: 0, y: 0, z: 0 })
+        body.current.setAngvel({ x: 0, y: 0, z: 0 })
     }
-    
-        useEffect(() => 
-        {
-            const unsubscribeReset = useGame.subscribe(
-                (state) => state.phase,
-                (phase) => {
-                    if(phase === 'ready')
-                        reset()
-                }
-            )
 
-            const unsubscribeJump = subscribeKeys(
-                (state) => state.jump,
-                (value) =>
-                {
-                    if(value)
-                        jump()
-                }
-            )
-
-            const unsubscribeAny = subscribeKeys(() =>
-            {
-                start()
-            })
-
-            return () =>
-            {
-                unsubscribeReset()
-                unsubscribeJump()
-                unsubscribeAny()
+    useEffect(() => {
+        const unsubscribeReset = useGame.subscribe(
+            state => state.phase,
+            phase => {
+                if (phase === 'ready') reset()
             }
+        )
 
-        }, [])
+        const unsubscribeJump = subscribeKeys(
+            state => state.jump,
+            value => {
+                if (value) jump()
+            }
+        )
 
-        useFrame((state, delta) =>
-        {
+        const unsubscribeAny = subscribeKeys(() => {
+            start()
+        })
+
+        return () => {
+            unsubscribeReset()
+            unsubscribeJump()
+            unsubscribeAny()
+        }
+    }, [])
+
+    useFrame((state, delta) => {
         // Controls
-            const { forward, backward, leftward, rightward } = getKeys()
+        const { forward, backward, leftward, rightward } = getKeys()
 
-            const impulse = {x: 0, y: 0, z: 0}
-            const torque = {x: 0, y: 0, z: 0}
+        const impulse = { x: 0, y: 0, z: 0 }
+        const torque = { x: 0, y: 0, z: 0 }
 
-            const impulseStrength = 0.6 * delta
-            const torqueStrength = 0.2 * delta
+        const impulseStrength = 0.6 * delta
+        const torqueStrength = 0.2 * delta
 
-            if(forward) {
-                impulse.z -= impulseStrength
-                torque.x -= torqueStrength
-            }
+        if (forward) {
+            impulse.z -= impulseStrength
+            torque.x -= torqueStrength
+        }
 
-            if(backward) {
-                impulse.z += impulseStrength
-                torque.x += torqueStrength
-            }
+        if (backward) {
+            impulse.z += impulseStrength
+            torque.x += torqueStrength
+        }
 
-            if(leftward) {
-                impulse.x -= impulseStrength
-                torque.z += torqueStrength
-            }
+        if (leftward) {
+            impulse.x -= impulseStrength
+            torque.z += torqueStrength
+        }
 
-            if(rightward) {
-                impulse.x += impulseStrength
-                torque.z -= torqueStrength
-            }
+        if (rightward) {
+            impulse.x += impulseStrength
+            torque.z -= torqueStrength
+        }
 
-            body.current.applyImpulse(impulse)
-            body.current.applyTorqueImpulse(torque)
+        body.current.applyImpulse(impulse)
+        body.current.applyTorqueImpulse(torque)
 
         // Camera
-            const bodyPosition = body.current.translation()
-            
-            const cameraPosition = new THREE.Vector3()
-            cameraPosition.copy(bodyPosition)
-            cameraPosition.z += 2.25
-            cameraPosition.y += 0.65
+        const bodyPosition = body.current.translation()
 
-            const cameraTarget = new THREE.Vector3()
-            cameraTarget.copy(bodyPosition)
-            cameraTarget.y += 0.25
+        const cameraPosition = new THREE.Vector3()
+        cameraPosition.copy(bodyPosition)
+        cameraPosition.z += 2.25
+        cameraPosition.y += 0.65
 
-            smoothedCameraPosition.lerp(cameraPosition, 5 * delta)
-            smoothedCameraTarget.lerp(cameraTarget, 5 * delta)
+        const cameraTarget = new THREE.Vector3()
+        cameraTarget.copy(bodyPosition)
+        cameraTarget.y += 0.25
 
-            state.camera.position.copy(smoothedCameraPosition)
-            state.camera.lookAt(smoothedCameraTarget)
+        smoothedCameraPosition.lerp(cameraPosition, 5 * delta)
+        smoothedCameraTarget.lerp(cameraTarget, 5 * delta)
+
+        state.camera.position.copy(smoothedCameraPosition)
+        state.camera.lookAt(smoothedCameraTarget)
 
         // Phases
-            if(bodyPosition.z < - (blocksCount * 4 + 2))
-                end()
+        if (bodyPosition.z < -(blocksCount * 4 + 2)) end()
 
-            if(bodyPosition.y < - 4)
-               restart()
+        if (bodyPosition.y < -4) restart()
+    })
 
-        })
-   
-    return <>
-    <RigidBody
-        ref={body}
-        canSleep={false}
-        position={[0, 1, 0]}
-        colliders="ball"
-        restitution={0.2}
-        friction={1}
-        linearDamping={0.5}
-        angularDamping={0.5}
-    >
-        <mesh
-            castShadow
-        >
-        {/* icosahedronGeometry to get a sphere with the triangles having near the same size  */}
-            <icosahedronGeometry args={ [0.3, 1]} />
-            <meshStandardMaterial 
-                flatShading 
-                color='mediumpurple'
-            />
-        </mesh>
-    </RigidBody>
-    </>
-
+    return (
+        <>
+            <RigidBody
+                ref={body}
+                canSleep={false}
+                position={[0, 1, 0]}
+                colliders="ball"
+                restitution={0.2}
+                friction={1}
+                linearDamping={0.5}
+                angularDamping={0.5}
+            >
+                <mesh castShadow>
+                    {/* icosahedronGeometry to get a sphere with the triangles having near the same size  */}
+                    <icosahedronGeometry args={[0.3, 1]} />
+                    <meshStandardMaterial flatShading color="mediumpurple" />
+                </mesh>
+            </RigidBody>
+        </>
+    )
 }
